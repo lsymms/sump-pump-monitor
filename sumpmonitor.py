25cc776c483a557d359b9a04b60c0a6c1e71e45c
root <root@raspberrypi>
2021-11-17 13:45:15 +0000

    added 30s wait after sending emails

--- sumpmonitor.py
+++ sumpmonitor.py
@@ -7,6 +7,7 @@ import time
 import logging
 
 # SYSTEM REQUIREMENTS: mail.mailutils and configured for gmail relay e.g. https://wiki.debian.org/GmailAndExim4
+# configured in /etc/ssmtp/ssmtp.conf
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -15,6 +16,8 @@ handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
+
+textemail = "3014486005@vtext.com,larrucifer@gmail.com,lsymms@gmail.com"
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
@@ -28,11 +31,13 @@ def alarmThread():
         	if ( GPIO.input(23) == False and pressed == False):
 			pressed = True
 			logger.info("sending alarm text" + '\n')
-                	os.system('echo "SUMP PUMP ALARM!!!" | mail -s "SUMP PUMP ALARM!!!" 3014486005@vtext.com  &')
+                	os.system('echo "SUMP PUMP ALARM!!!" | mail -s "SUMP PUMP ALARM!!!" '+ textemail +'  &')
+	        	time.sleep(30)
         	elif ( GPIO.input(23) == True and pressed == True):
 			pressed = False 
 			logger.info("alarm has turned off" + '\n')
-                	os.system('echo "Sump Pump Normal" | mail -s "Sump Pump Normal" 3014486005@vtext.com  &')
+                	os.system('echo "Sump Pump Normal" | mail -s "Sump Pump Normal" '+ textemail +'   &')
+	        	time.sleep(30)
 		else:
 			logger.debug("alarm thread: situation normal" + '\n')
         	time.sleep(0.1)
@@ -52,14 +57,19 @@ timeBetweenMonitorUpdates = datetime.timedelta(minutes=15);
 lastMonitorFileTouch = datetime.datetime.now() - timeBetweenMonitorUpdates;
 
 logger.info("Starting monitoring of Sump Pump")
-os.system('echo "Starting monitoring of Sump Pump" | mail -s "Starting monitoring of Sump Pump" larrucifer@gmail.com  &')
+os.system('echo "Starting monitoring of Sump Pump" | mail -s "Starting monitoring of Sump Pump" '+ textemail +'   &')
 
 while True:
 	now = datetime.datetime.now()
 
 	if ( (now - lastMonitorFileTouch) > timeBetweenMonitorUpdates ): 
-		logger.debug("touching sumpMonitor.touch file" + '\n')
-		os.system('touch /mnt/zfs500raidz/sumpMonitor.touch')
+
+                exists = os.path.isfile('/mnt/zfs500raidz/sumpMonitorMonitor.sh')
+                if exists:
+                        logger.debug("touching sumpMonitor.touch file" + '\n')
+                        os.system('touch /mnt/zfs500raidz/sumpMonitor.touch')
+                else:
+                        logger.debug("zfs not mounted or unable to connect to nfs" + '\n')
 
 	if ( now.time() > monitorTime and sendOkMsg == False):
 		sendOkMsg = True
